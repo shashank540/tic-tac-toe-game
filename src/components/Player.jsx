d42e2ff51bedf44b43df75b5ca8f81660047909f
shashank540 <shashank.srivastava5@gmail.com>
2026-09-08 23:05:08 +0530

    Initial commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,32 @@
+import {useState} from 'react';
+
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    let handleEditClick= () =>{
+        setIsEditing(!isEditing);
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editibalePlayerName = <span className="player-name">{playerName}</span>;
+    //let buttonCaption = "Edit";
+    if(isEditing) {
+        //editibalePlayerName = <input type="text" value={playerName} onChange={(e) => setPlayerName(e.target.value)}/>; //this will also works same as onchage in oneliner code
+        editibalePlayerName = <input type="text" value={playerName} onChange={handleChange}/>; //this will also works same as onchage with additional function handleChange
+    }
+    
+
+  return (
+    <li>
+      <span className="player">
+        {editibalePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  )
+}
